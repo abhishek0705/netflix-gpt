f452a3548f547e945aedde2e6c07c747ee031b0c
Abhishek Natani <abhisheknatani@mplgaming.com>
2024-08-17 19:19:41 +0530

    env fix

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -2,8 +2,8 @@ import OpenAI from "openai";
 import { OPENAI_API_KEY } from "./constant";
 
 const openai = new OpenAI({
-  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
-  // dangerouslyAllowBrowser: true,
+  apiKey: OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
 });
 
 export default openai;
